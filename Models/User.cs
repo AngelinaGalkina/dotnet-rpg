b8eb96a2ac9c7b74c88c2f47c6a64eb85f4f8e5a
Angelina <angelinagalkina93@gmail.com>
2023-04-04 21:06:51 +0300

    Added User model and User prop in the character

--- Models/User.cs
+++ Models/User.cs
@@ -6,5 +6,6 @@
         public string UserName { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
         public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+        public List<Character>? Characters { get; set; }
     }
 }
