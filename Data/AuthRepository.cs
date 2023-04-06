4c3cbd3e3b8e4d6401a892f51903bfe0a84dccc3
Angelina <angelinagalkina93@gmail.com>
2023-04-06 12:13:33 +0300

    Added AuthRepository and IAuthRepository

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext Context;
+        public AuthRepository(DataContext dataContext)
+        {
+            Context = dataContext;
+        }
+
+        public Task<ServiceResponse<string>> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            Context.Users.Add(user);
+            
+            await Context.SaveChangesAsync();
+
+            var response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            
+            return response;
+        }
+
+        public Task<bool> UserExists(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
