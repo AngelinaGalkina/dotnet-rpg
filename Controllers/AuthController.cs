4b8a241429ca993e3d890a0710ce264d14c6413e
Angelina <angelinagalkina93@gmail.com>
2023-04-07 15:26:46 +0300

    Added auth controller and login + register logic

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository AuthRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            AuthRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await AuthRepo.Register(
+                new User { UserName = request.UserName }, request.Password
+            );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+        {
+            var response = await AuthRepo.Login(request.UserName, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -8,35 +8,79 @@
             Context = dataContext;
         }
 
-        public Task<ServiceResponse<string>> Login(string userName, string password)
+        public async Task<ServiceResponse<string>> Login(string userName, string password)
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<string>();
+            var user = await Context.Users
+                .FirstOrDefaultAsync(u => u.UserName.ToLower().Equals(userName.ToLower()));
+
+            if (user is null)
+            {
+                response.Success = false;
+                response.Message = "User not found";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Password is wrong";
+            }
+            else
+            {
+                response.Data = user.Id.ToString();
+            }
+
+            return response;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
+            var response = new ServiceResponse<int>();
+
+            if (await UserExists(user.UserName))
+            {
+                response.Success = false;
+                response.Message = "User already exists";
+
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
             Context.Users.Add(user);
-            
             await Context.SaveChangesAsync();
 
-            var response = new ServiceResponse<int>();
             response.Data = user.Id;
-            
+
             return response;
         }
 
-        public Task<bool> UserExists(string userName)
+        public async Task<bool> UserExists(string userName)
         {
-            throw new NotImplementedException();
+            if (await Context.Users.AnyAsync(u => u.UserName.ToLower() == userName.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            return computedHash.SequenceEqual(passwordHash);
         }
     }
 }
